43ef53e4c6231dbb2080ace84ede31bd77bfc6a4
Athiqur01 <athik.apece@gmail.com>
2024-06-03 18:47:08 +0600

    update deshboard

--- src/Pages/DeshBoardPage/DeshBoardPage.jsx
+++ src/Pages/DeshBoardPage/DeshBoardPage.jsx
@@ -1,4 +1,4 @@
-
+import { FaHouseUser } from "react-icons/fa";
 
 const DeshBoardPage = () => {
     return (
@@ -6,7 +6,7 @@ const DeshBoardPage = () => {
             <div className="w-[30%] min-h-screen bg-orange-100 pl-2 md:pl-6 lg:pl-10">
             <h2 className="font-bold  my-6 lg:text-2xl">Ome's Heaven</h2>
                 <ul>
-                    <li className="font-bold">My Profile</li>
+                    <li className="font-bold"><FaHouseUser /> My Profile</li>
                     <li className="font-bold">Announcements</li>
                     
                 </ul>
